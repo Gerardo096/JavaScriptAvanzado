3f571a113d3fc60ad6fb09222442c03dab74e754
Gerardo096 <gerardopsb96@gmail.com>
2022-01-13 17:03:06 +0100

    medio empezado

--- P1/script.js
+++ P1/script.js
@@ -1,10 +1,9 @@
-function cambiarimg(pulsacion) {
-    var bt = pulsacion.target;
+document.addEventListener('DOMContentLoaded', function() {
+    button.addEventListener("click", ocultarForm());
 
-}
+    function ocultarForm() {
+        document.getElementById('formulario').style.display = 'none';
+        document.getElementById('tablero').style.display = 'block';
+    }
 
-
-//De esta forma detectamos cuando se pulsa en algun boton y lo manda en forma de array
-//que vamos a recorrer con un for each, vamos a estar a la espera de un click para llamar a 
-//la funcion que cambie la imagen segun si es el jugador 1 con una X o el jugador 2 con un O
-document.querySelectorAll('button').forEach(boton => boton.addEventListener('click', cambiarimg));
+});
